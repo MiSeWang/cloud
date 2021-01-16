48ce38eefbdf4eb64b8deb4ee0cd120b65f5f3e7
lvshiyu <826303461@qq.com>
2021-01-16 14:58:42 +0800

    项目初始化。

---
+++ config/src/test/java/com/mrlv/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrlv.config;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ eureka/src/test/java/com/mrlv/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrlv;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

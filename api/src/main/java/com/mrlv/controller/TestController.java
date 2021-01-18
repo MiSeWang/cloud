32b2db76340a68b2b9bab579cf4f11fe23fdaa2e
lvshiyu <826303461@qq.com>
2021-01-18 09:50:43 +0800

    初始化api

---
+++ api/src/main/java/com/mrlv/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.mrlv.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping("/index")
+    public String index(){
+//        return profile;
+        return "";
+    }
+}
---
+++ api/src/test/java/com/mrlv/api/ApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.mrlv.api;
+
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

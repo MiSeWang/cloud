53b36e634ba2544ea3b83b9fa7ec8cd83c10c365
lvshiyu <826303461@qq.com>
2021-02-22 17:19:09 +0800

    提交代码

--- api/src/main/java/com/mrlv/controller/TestController.java
+++ api/src/main/java/com/mrlv/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.mrlv.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
-    @Value("${profile}")
+    @Value("${spring.cloud.config.profile}")
     private String profile;
+    @Value("${tttt.spring.profiles}")
+    private String profile2;
 
     @GetMapping("/index")
     public String index(){
--- config/src/main/java/com/mrlv/ConfigApplication.java
+++ config/src/main/java/com/mrlv/ConfigApplication.java
@@ -2,12 +2,19 @@ package com.mrlv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
+/**
+ * 从Spring Cloud Edgware开始，@EnableDiscoveryClient 或@EnableEurekaClient 可省略。只需加上相关依赖，并进行相应配置，即可将微服务注册到服务发现组件上。
+ * @EnableDiscoveryClient和@EnableEurekaClient共同点就是：都是能够让注册中心能够发现，扫描到改服务。
+ * 不同点：@EnableEurekaClient只适用于Eureka作为注册中心，@EnableDiscoveryClient 可以是其他注册中心。
+ **/
 public class ConfigApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
     }
